f662df19e6741231de2cf8af73f8bcd3a547a3aa
Vibha Shivarajan <123540046+vibhashivarajan@users.noreply.github.com>
2023-01-26 14:07:53 -0800

    Update OrderItemRepository.java

--- src/main/java/org/isd/ihs/decasims/repository/OrderItemRepository.java
+++ src/main/java/org/isd/ihs/decasims/repository/OrderItemRepository.java
@@ -4,7 +4,8 @@ import org.isd.ihs.decasims.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * The Interface OrderItemRepository.
+ * The Interface OrderItemRepository:  java interface which represents an access to
+ * database table of OrderItem (sprignboot jpa feature)
  */
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long>	{
 
