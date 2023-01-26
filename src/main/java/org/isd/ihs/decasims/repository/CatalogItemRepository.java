77824a2d4d038a36940e4dec0ee4c5e3b7b0a517
Vibha Shivarajan <123540046+vibhashivarajan@users.noreply.github.com>
2023-01-26 14:07:30 -0800

    Update CatalogItemRepository.java

--- src/main/java/org/isd/ihs/decasims/repository/CatalogItemRepository.java
+++ src/main/java/org/isd/ihs/decasims/repository/CatalogItemRepository.java
@@ -4,7 +4,8 @@ import org.isd.ihs.decasims.entity.CatalogItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * The Interface CatalogItemRepository.
+ * The Interface CatalogItemRepository: java interface which represents an access to
+ * database table of CatalogItem (sprignboot jpa feature)
  */
 public interface CatalogItemRepository extends JpaRepository<CatalogItem, Long>{
 
