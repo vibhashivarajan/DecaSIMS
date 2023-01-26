08741c542de7086dbf9b89e30c2e1ca74132efb3
Vibha Shivarajan <123540046+vibhashivarajan@users.noreply.github.com>
2023-01-26 14:17:00 -0800

    Update PasswordGenerator.java

--- src/main/java/org/isd/ihs/decasims/util/PasswordGenerator.java
+++ src/main/java/org/isd/ihs/decasims/util/PasswordGenerator.java
@@ -3,7 +3,10 @@ package org.isd.ihs.decasims.util;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
- * The Class PasswordGenerator.
+ * The Class PasswordGenerator: Code not directly related or used by deca sims project. A standalone
+ * class to generate encoded databse password (as we don't want to store, plain text database password 
+ * in the database)
+ * 
  */
 public class PasswordGenerator {
 
